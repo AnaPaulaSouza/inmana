239c575b8c3233cd07c23b29bf60b6a92922c968
Ana Paula Souza <ana.pdsouza@b2wdigital.com>
2021-04-20 17:23:34 -0300

    Aula 01 - Liftoff

---
+++ lib/inmana_web/router.ex
@@ -0,0 +1,22 @@
+defmodule InmanaWeb.Router do
+  use InmanaWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", InmanaWeb do
+    pipe_through :api
+
+    get "/", WelcomeController, :index
+  end
+
+  if Mix.env() in [:dev, :test] do
+    import Phoenix.LiveDashboard.Router
+
+    scope "/" do
+      pipe_through [:fetch_session, :protect_from_forgery]
+      live_dashboard "/dashboard", metrics: InmanaWeb.Telemetry
+    end
+  end
+end
